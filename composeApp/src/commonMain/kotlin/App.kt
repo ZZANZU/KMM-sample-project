f63e4533b4b7f9404fde584b8f30fade1495a8c8
ZZANZU <lees3301@gmail.com>
2023-11-13 21:07:22 +0900

    add TextField

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -2,22 +2,27 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.*
-import kotlinx.datetime.Clock
-import kotlinx.datetime.LocalDateTime
-import kotlinx.datetime.TimeZone
-import kotlinx.datetime.toLocalDateTime
+import kotlinx.datetime.*
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
 fun App() {
     MaterialTheme {
+        var location by remember { mutableStateOf("Europe/Paris") }
         var timeAtLocation by remember { mutableStateOf("No location selected") }
         Column {
             Text(timeAtLocation)
+            TextField(
+                value = location,
+                onValueChange = {
+                    location = it
+                }
+            )
             Button(onClick = {
-                timeAtLocation = "13:30"
+                timeAtLocation = currentTimeAt(location) ?: "Invalid location"
             }) {
                 Text("Show Time At Location")
             }
@@ -25,11 +30,15 @@ fun App() {
     }
 }
 
-fun todaysDate(): String {
-    fun LocalDateTime.format() = toString().substringBefore('T')
+fun currentTimeAt(location: String): String? {
+    fun LocalTime.formatted() = "$hour:$minute:$second"
 
-    val now = Clock.System.now()
-    val zone = TimeZone.currentSystemDefault()
-
-    return now.toLocalDateTime(zone).format()
+    return try {
+        val time = Clock.System.now()
+        val zone = TimeZone.of(location)
+        val localTime = time.toLocalDateTime(zone).time
+        "${location}의 시간은 ${localTime.formatted()}"
+    } catch (e: IllegalTimeZoneException) {
+        null
+    }
 }
